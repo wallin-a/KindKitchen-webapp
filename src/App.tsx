5690eaea90d63d45fd84e77c3e94931abbe29201
anna <anna.wallin@imaginecare.com>
2025-04-27 10:41:10 +0200

    add react app, set up with webpack and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">
+        Hello, Webpack + React + Tailwind + TypeScript!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
